a8a7dcb09eb95024c0ec6e800948a1d656c6e464
ariasDev <57456319+ariasDev@users.noreply.github.com>
2020-09-20 17:40:58 -0500

    refactor

--- src/app/components/registry-component/registry-component.component.ts
+++ src/app/components/registry-component/registry-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {MainServiceService} from '../../main-service.service'
-import { Subscription} from 'rxjs';
+import { MainServiceService } from '../../main-service.service'
+import { Subscription } from 'rxjs';
 import { ageValidator } from '../../utils/customValidators'
 
 @Component({
@@ -15,49 +15,49 @@ export class RegistryComponentComponent implements OnInit, OnDestroy {
   suscribeGet: Subscription;
   suscribePost: Subscription;
 
-  constructor(private formBuilder: FormBuilder, private mainService:MainServiceService) {
+  constructor(private formBuilder: FormBuilder, private mainService: MainServiceService) {
     this.checkoutForm = this.formBuilder.group({
       birthdate: new FormControl('', [Validators.required, ageValidator(18)]),
       firstname: new FormControl('', [Validators.required, Validators.minLength(4)]),
       lastname: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      identification: new FormControl('', [Validators.required, Validators.minLength(10),])
+      identification: new FormControl('', [Validators.required, Validators.min(100000), Validators.max(9999999999999999)])
     });
 
   }
 
-    onSubmit(userData) {
-      if(userData.birthdate && userData.firstname && userData.lastname && userData.identification){
-        if(this.checkoutForm.valid){
-          this.suscribeGet = this.mainService.getAllClients().subscribe(response=>{
-              const responseString = JSON.stringify(response)
-              if(responseString.indexOf(`identification":"${userData.identification}`) === -1){
-                this.suscribePost = this.mainService.registerNewCustomer(userData).subscribe(response => {
-                  alert('Usurio Registrado con exito')
-                })
-              } else {
-                alert('Este usuario ya se encuentra registrado')
-              }
-              
-          })
-            this.checkoutForm.reset();
-        } else {
-          alert('Algunos Datos que diligenciaste no son validos')
-        }
+  onSubmit(userData) {
+    if (userData.birthdate && userData.firstname && userData.lastname && userData.identification) {
+      if (this.checkoutForm.valid) {
+        this.suscribeGet = this.mainService.getAllClients().subscribe(response => {
+          const responseString = JSON.stringify(response)
+          if (responseString.indexOf(`identification":"${userData.identification}`) === -1) {
+            this.suscribePost = this.mainService.registerNewCustomer(userData).subscribe(response => {
+              alert('Usurio Registrado con exito')
+            })
+          } else {
+            alert('Este usuario ya se encuentra registrado')
+          }
 
+        })
+        this.checkoutForm.reset();
       } else {
-        alert('Todos los campos deben estar Diligenciados')
+        alert('Algunos Datos que diligenciaste no son validos')
       }
+
+    } else {
+      alert('Todos los campos deben estar Diligenciados')
     }
+  }
 
   ngOnInit(): void {
   }
 
-  get identification() {return this.checkoutForm.get('identification')}
-  get firstname() {return this.checkoutForm.get('firstname')}
-  get lastname() {return this.checkoutForm.get('lastname')}
-  get birthdate() {return this.checkoutForm.get('birthdate')}
+  get identification() { return this.checkoutForm.get('identification') }
+  get firstname() { return this.checkoutForm.get('firstname') }
+  get lastname() { return this.checkoutForm.get('lastname') }
+  get birthdate() { return this.checkoutForm.get('birthdate') }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.suscribeGet.unsubscribe()
     this.suscribePost.unsubscribe()
   }
